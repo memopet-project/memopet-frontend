8ddb607d0f3e7fc31f595c4c568af78f73aa7c3f
JUNG GIL WOONG <91588779+DoggySummer@users.noreply.github.com>
2023-10-23 09:31:32 +0900

    🐛 figspec 버그수정 2

--- stories/Buttons.stories.ts
+++ stories/Buttons.stories.ts
@@ -33,7 +33,7 @@ Primary.parameters = {
   design: {
     type: 'figspec',
     url: 'https://www.figma.com/file/ZYSDCk1qmBQ8CFhHZBYPLG/JS-Project?type=design&node-id=11%3A7&mode=design&t=iNtpjRcL7BhD0gRd-1',
-    accessToken: process.env.NEXT_PUBLIC_STORYBOOK_FIGMA_ACCESS_TOKEN
+    accessToken: "figd_AkjabgaEZDojm4_r3JaRjGB4gp2hpXLcdcMXk073"
   }
 }
 Warning.parameters = {
