c99c6df3608b83bcbe774a0e1d0e2493ac1a8f61
JUNG GIL WOONG <91588779+DoggySummer@users.noreply.github.com>
2023-12-11 18:06:05 +0900

    carousel 처리

--- app/components/button/movePictureBtn.tsx
+++ app/components/button/movePictureBtn.tsx
@@ -1,11 +1,34 @@
 import PicRightSVG from '@/public/svg/picRight.svg'
+import PicLeftSVG from '@/public/svg/picLeft.svg'
+import { useSwiper } from 'swiper/react'
 
-const MovePictureBtn = () => {
-  return (
-    <button className="w-12 h-12 flex items-center justify-center rounded-full border border-solid border-gray07 bg-gray02 hover:bg-gray03">
-      <PicRightSVG />
-    </button>
-  )
+interface buttonType {
+  buttonType: 'prev' | 'next'
+}
+
+const MovePictureBtn = ({buttonType}:buttonType) => {
+  const swiper = useSwiper();
+ 
+
+  if (buttonType==='prev') {
+    return (
+      <button 
+        className="ml-2 cursor-pointer z-40 absolute top-1/2 flex items-center justify-center rounded-full"
+        onClick={()=> swiper.slidePrev()}
+      >
+        <PicLeftSVG className='opacity-50 hover:opacity-80 '/>
+      </button>
+    )
+  } else {
+    return (
+      <button 
+        className="mr-2 cursor-pointer flex items-center absolute top-1/2 z-40 right-0 justify-center rounded-full"
+        onClick={()=> swiper.slideNext()}
+      >
+        <PicRightSVG className='opacity-50 hover:opacity-80 '/>
+      </button>
+    )
+  }
 }
 
 export default MovePictureBtn
--- app/components/memory/photoArea.tsx
+++ app/components/memory/photoArea.tsx
@@ -1,29 +1,45 @@
+import Image from "next/image";
 import {Swiper, SwiperSlide } from "swiper/react"
+import { Navigation, Pagination, A11y } from "swiper/modules";
+
 import 'swiper/css' 
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
-import useCarousel from "@/app/utils/useCarousel";
+
+import MovePictureBtn from '../button/movePictureBtn';
+
 
 const PhotoArea = ({ photos }:{photos: string[]}) => {
   const tempPhotos = [
-    'aa', 'bb', 'cc','dd'
+    'https://www.princeton.edu/sites/default/files/styles/1x_full_2x_half_crop/public/images/2022/02/KOA_Nassau_2697x1517.jpg?itok=Bg2K7j7J', 
+    'https://hips.hearstapps.com/ghk.h-cdn.co/assets/16/08/gettyimages-530330473.jpg?crop=0.659xw:0.990xh;0.123xw,0.00779xh&resize=980:*', 
+    'https://thumbor.forbes.com/thumbor/fit-in/900x510/https://www.forbes.com/advisor/wp-content/uploads/2023/07/top-20-small-dog-breeds.jpeg.jpg',
   ]
-  const { currentIndex, goToNextSlide, goToPrevSlide } = useCarousel(tempPhotos)
+
   return (
-    <div className="carousel">
-      <button onClick={goToPrevSlide}>이전</button>
-      <div className="slide-container">
-        {photos.map((item, index:number) => (
-          <div
-            key={index}
-            className={`slide ${index === currentIndex ? 'active' : ''}`}
-          >
-            {item}
-          </div>
-        ))}
-      </div>
-      <button onClick={goToNextSlide}>다음</button>
+    <div className="flex items-center justify-center">
+      <Swiper
+        navigation
+        pagination
+        loop
+        modules={[Navigation, Pagination, A11y]}
+        className="max-w-[400px] h-[300px] flex items-center justify-center"
+      >
+        {
+          tempPhotos.map((items, i )=>{
+           return (
+            <SwiperSlide
+              className=""
+              key={i}>
+              <Image src={items} alt="dog" width={100} height={100} className="w-full"/>
+            </SwiperSlide>
+           ) 
+          })
+        }
+        <MovePictureBtn buttonType='prev' />
+        <MovePictureBtn buttonType='next' />
+      </Swiper>
     </div>
   )
 }
