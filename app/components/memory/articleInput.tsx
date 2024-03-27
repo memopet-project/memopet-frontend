134d1e0fcb862e19fd69a6200a394102adf87667
eunjin0212 <eunjin9639@gmail.com>
2024-03-27 18:54:46 +0900

    ⚰️ Remove dead code

--- app/components/memory/articleInput.tsx
+++ app/components/memory/articleInput.tsx
@@ -1,23 +1,23 @@
 import { ChangeEvent, useState } from 'react'
-import RoundedBtn from "../button/roundedBtn"
+import RoundedBtn from '../button/roundedBtn'
 import { commentInfoType, userInfoType } from '@/app/interfaces'
 
 interface InputProps {
   userInfo: userInfoType,
-  addComment: ({}:commentInfoType)=>void
+  addComment: ({ }: commentInfoType) => void
 }
 
 const ArticleInput = ({
-    userInfo,
-    addComment
-  }:InputProps) => {
+  userInfo,
+  addComment
+}: InputProps) => {
   const [inputValue, setInputValue] = useState('')
   const [isFocused, setIsFocused] = useState(false)
-  const handleInputChange = (e:ChangeEvent<HTMLInputElement>) => {
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value)
   }
 
-  const handleFocusChange = (focusStatus:boolean) => {
+  const handleFocusChange = (focusStatus: boolean) => {
     setIsFocused(focusStatus)
   }
 
@@ -26,9 +26,9 @@ const ArticleInput = ({
   }
 
   const submitReply = () => {
-    if(inputValue.length === 0){
+    if (inputValue.length === 0) {
       alert('댓글이 비어있습니다.')
-    } 
+    }
     else {
       addComment({
         id: Math.floor(Math.random() * 100),
@@ -41,28 +41,25 @@ const ArticleInput = ({
   };
   const handleKeyDown = (e: React.KeyboardEvent) => {
     if (e.key === 'Enter') {
-      submitReply(); 
+      submitReply();
     }
   };
 
   return (
     <div className='relative w-[400px]'>
-      <input 
+      <input
         placeholder='답글을 작성해 주세요'
-        onFocus={()=> handleFocusChange(true)}
-        onBlur={()=> handleFocusChange(inputValue!=='')}
+        onFocus={() => handleFocusChange(true)}
+        onBlur={() => handleFocusChange(inputValue !== '')}
         type='text'
-        className="
-          mb-2 px-3 text-base font-normal flex items-center border border-solid border-gray03 rounded-md w-full h-14 font-pretendard
-          focus:outline-none focus:border-red05
-        "
-        value={inputValue} 
+        className='mb-2 px-3 text-base font-normal flex items-center border border-solid border-gray03 rounded-md w-full h-14 font-pretendard focus:outline-none focus:border-red05'
+        value={inputValue}
         onChange={handleInputChange}
         onKeyDown={handleKeyDown}
       />
       <div className={`flex gap-1 justify-end ${isFocused ? 'block' : 'hidden'}`}>
-        <RoundedBtn buttonType="white02" text="취소" onClick={cancelReply}/>
-        <RoundedBtn buttonType={inputValue.length ? 'red01' : 'gray01'} text="등록"  onClick={submitReply}/>
+        <RoundedBtn buttonType='white02' text='취소' onClick={cancelReply} />
+        <RoundedBtn buttonType={inputValue.length ? 'red01' : 'gray01'} text='등록' onClick={submitReply} />
       </div>
     </div>
   )
