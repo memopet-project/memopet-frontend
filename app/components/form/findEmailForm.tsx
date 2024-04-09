b0166256ca38a6a282c3a65c0a13ddc79509ed88
eunjin0212 <eunjin9639@gmail.com>
2024-04-10 00:10:06 +0900

    🎨 이메일 찾기 api 연결

--- app/components/form/findEmailForm.tsx
+++ app/components/form/findEmailForm.tsx
@@ -1,48 +1,134 @@
 import { type FormEvent, useMemo, useState } from 'react'
 import MainBtn from '../button/mainBtn'
 import ValidationInput from '../input/validationInput'
-import Google from '@/public/images/google.png'
-import type { ChangeEvt } from '@/app/types/common'
+// import Google from '@/public/images/google.png'
+import type { ChangeEvt, ValidateObj } from '@/app/types/common'
 import Image from 'next/image'
+import api from '@/app/api/axios'
+import { initValidateObj } from '@/app/constants/login'
+import checkName from '@/app/utils/checkName'
+import checkContactNumber from '@/app/utils/checkContactNumber'
+import { useSetRecoilState } from 'recoil'
+import { modalStatus } from '@/app/recoil/startModalStatus'
 
-type Result = null | { dsc_code: '0' | '1' | '2', email: string }
+interface ResponseType { dsc_code: '0' | '1' | '2', email: string }
+
+type Result = null | ResponseType
+
+const initFindEmailInfo = {
+  name: '',
+  contact: '',
+}
+interface Validate {
+  name: ValidateObj,
+  contact: ValidateObj,
+}
+
+const initValidate = {
+  name: { msg: '', status: null },
+  contact: { msg: '', status: null },
+} as const
+type ValidateKey = keyof typeof initValidate;
+
 
 const FindEmailForm = () => {
-  const [name, setName] = useState('')
-  const [contact, setPassword] = useState('')
+  const [findEmailInfo, setFindEmailInfo] = useState(initFindEmailInfo)
+  const [validate, setValidate] = useState<Validate>({ ...initValidate })
   const [result, setResult] = useState<Result>(null)
+  const setModalStatus = useSetRecoilState(modalStatus);
+
+  // FIXME: 중복코드
+  function initializeValidate(key: ValidateKey) {
+    setValidate((prev) => ({ ...prev, [key]: initValidateObj }))
+  }
+
+  // 유효성 검사 실패
+  function failValidate(key: ValidateKey, msg: string) {
+    setValidate((prev) => ({ ...prev, [key]: { msg: msg, status: false } }))
+  }
+
+  // 유효성 검사 성공
+  function successValidate(key: ValidateKey, msg = '') {
+    setValidate((prev) => ({ ...prev, [key]: { msg, status: true } }))
+  }
 
   const inputs = [
     {
       label: '이름',
       placeholder: '이름 입력',
-      type: 'name',
-      value: name,
+      type: 'text',
+      value: findEmailInfo.name,
       name: 'name',
+      validate: validate.name,
       onChange: (value: ChangeEvt) => {
-        setName(value)
+        setFindEmailInfo({ ...findEmailInfo, name: value })
         setResult(null)
+        initializeValidate('name')
       },
     },
     {
       label: '휴대폰번호',
       placeholder: "'-' 없이 숫자만 입력",
-      type: 'contact',
-      value: contact,
+      type: 'tel',
+      value: findEmailInfo.contact,
       name: 'contact',
+      validate: validate.contact,
       onChange: (value: ChangeEvt) => {
-        setPassword(value)
+        setFindEmailInfo({ ...findEmailInfo, contact: value })
         setResult(null)
+        initializeValidate('contact')
       },
     },
   ]
 
-  const disabled = useMemo(() => !(name && contact) || !!(name && contact && result?.dsc_code === '0'), [name, contact])
-  const buttonLabel = useMemo(() => result?.dsc_code === '1' ? '로그인하기' : result?.dsc_code === '2' ? '소셜 로그인하기' : '이메일 찾기', [name, contact])
+  const disabled = useMemo(() => !(findEmailInfo.name && findEmailInfo.contact) || !!(findEmailInfo.name && findEmailInfo.contact && result?.dsc_code === '0'), [findEmailInfo])
+  const buttonLabel = {
+    '0': '이메일 찾기',
+    '1': '로그인하기',
+    '2': '소셜 로그인하기'
+  }
+  async function findEmail() {
+    if (checkName(findEmailInfo.name)) {
+      failValidate('name', '한글만 입력해주세요. (영문, 특수기호 입력 불가)')
+      return
+    }
+    successValidate('name')
 
-  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    if (checkContactNumber(findEmailInfo.contact)) {
+      failValidate('contact', '숫자만 입력해주세요.')
+      return
+    }
+    successValidate('contact')
+
+    try {
+      const res = await api.post<ResponseType>('sign-in/my-id', {
+        username: findEmailInfo.name,
+        phone_num: findEmailInfo.contact,
+      })
+      setResult(res.data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  function handleClick() {
+    setModalStatus('findPassword')
+  }
+
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault()
-    setResult({ dsc_code: '2', email: 'eunjin9639@gmail.com' })
+
+    if (result?.dsc_code === '1') {
+      setModalStatus('emailLogin')
+      return;
+    }
+
+    if (result?.dsc_code === '2') {
+      setModalStatus('start')
+      return
+    }
+
+    findEmail()
   }
 
   return (
@@ -57,6 +143,7 @@ const FindEmailForm = () => {
           name={input.name}
           type={input.type}
           onChange={input.onChange}
+          validate={input.validate}
         />
       ))}
       {result?.dsc_code && (result?.dsc_code === '0'
@@ -77,10 +164,13 @@ const FindEmailForm = () => {
               />
             </span>}{result?.email}
           </p>
-          {result?.dsc_code === '1' && <div className='other-options-box mt-4'><span>비밀번호가 생각나지 않나요?</span><button>비밀번호 찾기</button></div>}
+          {result?.dsc_code === '1' && <div className='other-options-box mt-4'>
+              <span>비밀번호가 생각나지 않나요?</span>
+              <button onClick={handleClick}>비밀번호 찾기</button>
+            </div>}
         </div>))
       }
-      <MainBtn text={buttonLabel} disabled={disabled} />
+      <MainBtn text={buttonLabel[result?.dsc_code || '0']} disabled={disabled} />
     </form>
   )
 }
