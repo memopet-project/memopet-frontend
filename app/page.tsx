4d4b530440aac86a4439127fd8eec202f8a50912
eunjin0212 <eunjin9639@gmail.com>
2024-04-11 00:24:34 +0900

    💄 Edit main style

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Login from './(pages)/login/page';
 /** 메인 페이지 */
 export default function Home() {
   return (
-    <main className="h-[2000px]">
+    <main>
       <Login />
     </main>
   )
