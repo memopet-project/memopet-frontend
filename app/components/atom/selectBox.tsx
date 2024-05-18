edb1766106f07cb17cfa2e0d2879aa6e41b6ab51
sh-oon <jshyh1@naver.com>
2024-05-19 01:31:02 +0900

    feat/ selectBox 컴포넌트 추가

---
+++ app/components/atom/selectBox.tsx
@@ -0,0 +1,148 @@
+'use client';
+
+import React, { useMemo, useRef, useState } from 'react';
+import SearchSVG from '@/public/svg/search.svg';
+import Image from 'next/image';
+
+type Props = {
+  id: string;
+  value: string;
+  placeholder: string;
+  options: string[];
+  onSelect: (value: string) => void;
+  canFocus?: () => boolean;
+}
+
+const SelectBox = ({ id, value, placeholder, options, onSelect, canFocus = () => true }: Props) => {
+  const [searchValue, setSearchValue] = useState<string>(value); // 검색어
+  const [isFocused, setIsFocused] = useState<boolean>(false);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [isOpenOptions, setIsOpenOptions] = useState<boolean>(false);
+
+  const containerClass = (): string => {
+    let commonClass = 'h-[52px] w-full relative flex border rounded-[6px] overflow-hidden px-3 py-[14px] gap-[10px]';
+
+    // 0 = default, 1 = focused
+    let inputState: number = 0;
+    if (isFocused) inputState = 1;
+
+    switch (inputState) {
+      case 0:
+        return `${commonClass} border-[#525252]`;
+      case 1:
+        return `${commonClass} border-[#F15139]`;
+      default:
+        return `${commonClass} border-[#525252]`;
+    }
+  };
+
+  const optionClass = (option: string): string => {
+    let commonClass = 'px-3 py-2 cursor-pointer hover:bg-redBlur';
+
+    if (option === '일치하는 결과가 없습니다.') return `${commonClass} text-gray04 pointer-events-none`;
+
+    return commonClass;
+  };
+
+  const charClass = (char: string, option: string): string => {
+    if (option === '일치하는 결과가 없습니다.') return 'text-gray04';
+
+    if (searchValue.length === 0) return 'text-inherit';
+
+    if (searchValue.includes(char) && option.includes(searchValue)) return 'text-red-500';
+
+    return 'text-inherit';
+  };
+
+  const sortedOptions = useMemo(() => {
+    if (searchValue.length === 0) return options;
+
+    // 검색어가 포함된 옵션만 필터링
+    const filteredOptions = options.filter((option) => option.includes(searchValue));
+
+    if (filteredOptions.length === 0) return ['일치하는 결과가 없습니다.'];
+
+    // 검색어가 포함된 옵션을 앞으로
+    return filteredOptions.sort((a, b) => a.indexOf(searchValue) - b.indexOf(searchValue));
+
+  }, [options, searchValue]);
+
+  const handleFocus = () => {
+    if (!canFocus()) {
+      inputRef.current?.blur();
+      return;
+    }
+    setIsFocused(true);
+    if (searchValue.length > 0) setIsOpenOptions(true);
+  };
+
+
+  return (
+    <>
+      <div className="relative">
+        <label htmlFor={id} className={containerClass()}>
+          <input
+            type="text"
+            className="w-full h-full outline-none"
+            value={searchValue}
+            placeholder={placeholder}
+            id={id}
+            ref={inputRef}
+            onChange={(e) => {
+              setSearchValue(e.target.value);
+              if (e.target.value.length > 0) setIsOpenOptions(true);
+              else setIsOpenOptions(false);
+            }}
+            onFocus={handleFocus}
+            onBlur={() => setIsFocused(false)}
+          />
+          {(searchValue.length > 0 && isOpenOptions) && (
+            <button
+              onClick={() => {
+                setSearchValue('');
+                setIsFocused(false);
+                setIsOpenOptions(false);
+              }}
+            >
+              <Image
+                src="/svg/close.svg"
+                alt="close"
+                width={24}
+                height={24}
+              />
+            </button>
+          )}
+          <SearchSVG />
+        </label>
+        {(isOpenOptions && options.length > 0) && (
+          <div className="absolute top-[56px] w-full bg-white border border-[#525252] rounded-[6px] overflow-hidden">
+            <ul className="flex flex-col max-h-40 overflow-auto">
+              {sortedOptions.map((option) => (
+                <li
+                  key={option}
+                  onClick={() => {
+                    onSelect(option);
+                    setSearchValue(option);
+                    setIsOpenOptions(false);
+                  }}
+                  className={optionClass(option)}
+                >
+                  {option.replace(' ', '').split('').map((char, index) => (
+                    <span
+                      key={index}
+                      className={charClass(char, option)}
+                    >
+                    {char}
+                  </span>
+                  ))}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default SelectBox;
--- app/components/molecules/fieldset/petTypeForm.tsx
+++ app/components/molecules/fieldset/petTypeForm.tsx
@@ -9,12 +9,10 @@ import SmallAnimalSVG from '@/public/svg/animal-type/small_animal.svg';
 import ReptileSVG from '@/public/svg/animal-type/reptile.svg';
 import AmphibianSVG from '@/public/svg/animal-type/amphibian.svg';
 import FootPrintSVG from '@/public/svg/animal-type/foot_print.svg';
+import SelectBox from '@/app/components/atom/selectBox';
+import type { TPetType } from '@/app/constants/petType';
+import IconBase from '@/app/components/atom/icon/iconBase';
 
-type TPetType = {
-  id: number;
-  name: string;
-  icon: (className: string) => JSX.Element;
-}
 
 type TPetTypeParam = {
   id: number;
@@ -27,36 +25,43 @@ export const petTypes: TPetType[] = [
     id: 1,
     name: '강아지',
     icon: (className) => <DogSVG fill={className} />,
+    species: ['푸들', '말티즈', '시츄', '요크셔테리어', '포메라니안', '치와와', '닥스훈트', '비글', '시바이누', '진돗개', '시베리안 허스키', '골든 리트리버', '래브라도 리트리버', '보더 콜리', '저먼 셰퍼드', '불독', '퍼그', '불테리어', '말라뮤트', '허스키', '말라노이즈', '프렌치 불독', '불리'],
   },
   {
     id: 2,
     name: '고양이',
     icon: (className) => <CatSVG fill={className} />,
+    species: ['샴', '페르시안', '러시안블루', '스코티쉬폴드', '먼치킨', '시암', '아비시니안', '뱅갈', '노르웨이숲', '러그돈', '스핑크스'],
   },
   {
     id: 3,
     name: '어류',
     icon: (className) => <PiscesSVG fill={className} />,
+    species: ['금붕어', '메추리', '콩고페더'],
   },
   {
     id: 4,
     name: '소동물',
     icon: (className) => <SmallAnimalSVG fill={className} />,
+    species: ['햄스터', '토끼', '고슴도치', '기니피그'],
   },
   {
     id: 5,
     name: '파충류',
     icon: (className) => <ReptileSVG fill={className} />,
+    species: ['뱀', '도마뱀', '거북이'],
   },
   {
     id: 6,
     name: '양서류',
     icon: (className) => <AmphibianSVG fill={className} />,
+    species: ['개구리', '도롱뇽'],
   },
   {
     id: 7,
     name: '기타',
     icon: (className) => <FootPrintSVG fill={className} />,
+    species: [],
   },
 ];
 
@@ -67,20 +72,9 @@ const PetTypeForm = () => {
     pet_spec_m: '',
     pet_spec_s: '',
   });
+  const [petTypeSpeciesError, setPetTypeSpeciesError] = useState<boolean>(false);
 
   const iconColor = (color: string, returnType: 'hex' | 'class'): string => {
-    // switch (color) {
-    //   case 'black':
-    //     return returnType === 'hex' ? '#404040' : 'text-gray07 border-gray07';
-    //   case 'gray':
-    //     return returnType === 'hex' ? '#A3A3A3' : 'text-gray04 border-gray04';
-    //   case 'red':
-    //     return returnType === 'hex' ? '#F15139' : 'text-red05 border-red05';
-    //   case 'white':
-    //     return returnType === 'hex' ? '#FFFFFF' : 'text-white border-white';
-    //   default:
-    //     return '';
-    // }
     if (returnType === 'hex') {
       switch (color) {
         case 'black':
@@ -113,37 +107,70 @@ const PetTypeForm = () => {
   };
 
   return (
-    <fieldset>
-      <span>종류</span>
-      <div className="grid grid-cols-4 grid-rows-2 gap-2">
-        {
-          petTypes.map((petType) => (
-            <label
-              htmlFor={'pet-type-' + petType.id}
-              key={petType.id}
-              className={iconColor(petTypeParam.id === petType.id ? 'black' : 'gray', 'class')}
-            >
-              <input
-                type="radio"
-                id={'pet-type-' + petType.id}
-                name="petType"
-                value={petType.id}
-                checked={petTypeParam.id === petType.id}
-                onChange={() => setPetTypeParam({
-                  ...petTypeParam,
-                  id: petType.id,
-                  pet_spec_m: petType.name,
-                })}
-                className="hidden"
-              />
-              <div className="flex flex-col items-center gap-2">
-                {petType.icon(iconColor(petTypeParam.id === petType.id ? 'black' : 'gray', 'hex'))}
-                <span>{petType.name}</span>
-              </div>
-            </label>
-          ))
-        }
+    <fieldset className="flex flex-col gap-2">
+      <div className="flex flex-col gap-1">
+        <span>종류</span>
+        <div className="grid grid-cols-4 grid-rows-2 gap-2">
+          {
+            petTypes.map((petType) => (
+              <label
+                htmlFor={'pet-type-' + petType.id}
+                key={petType.id}
+                className={iconColor(petTypeParam.id === petType.id ? 'black' : 'gray', 'class')}
+              >
+                <input
+                  type="radio"
+                  id={'pet-type-' + petType.id}
+                  name="petType"
+                  value={petType.id}
+                  checked={petTypeParam.id === petType.id}
+                  onChange={() => setPetTypeParam({
+                    ...petTypeParam,
+                    id: petType.id,
+                    pet_spec_m: petType.name,
+                  })}
+                  className="hidden"
+                />
+                <div className="flex flex-col items-center gap-2">
+                  {petType.icon(iconColor(petTypeParam.id === petType.id ? 'black' : 'gray', 'hex'))}
+                  <span>{petType.name}</span>
+                </div>
+              </label>
+            ))
+          }
+        </div>
       </div>
+      {petTypeSpeciesError && (
+        <div className="flex">
+          <IconBase
+            src={'/svg/icon-error.svg'}
+            alt={'errorIcon'}
+            width={16}
+            height={16}
+            size={'md'}
+          />
+          <span className="text-errorRed">종류를 먼저 선택해주세요!</span>
+        </div>
+      )}
+      <SelectBox
+        id="pet-species"
+        options={petTypes.find((petType) => petType.id === petTypeParam.id)?.species || []}
+        onSelect={(option) => setPetTypeParam({
+          ...petTypeParam,
+          pet_spec_s: option,
+        })}
+        value={petTypeParam.pet_spec_s}
+        placeholder="종류를 선택해주세요."
+        canFocus={() => {
+          if (petTypeParam.id === 0) {
+            setPetTypeSpeciesError(true);
+            return false;
+          } else {
+            setPetTypeSpeciesError(false);
+            return true;
+          }
+        }}
+      />
     </fieldset>
   );
 };
