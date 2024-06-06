82ad7dae293c9f675c0972757a856ac3e531095c
nueizkk <nueizkk@gmail.com>
2024-06-06 15:21:47 +0900

    chore / footer max-width 설정

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
 import { css } from '@emotion/react';
 import Link from 'next/link';
 
-const footerStyles = {
-  container: css`
+const styles = {
+  footer: css`
     height: 96px;
-    padding: 0 1rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `,
+  container: css`
+    width: 100%;
+    max-width: 1160px;
+    padding: 1rem;
     display: flex;
     justify-content: space-between;
     align-items: center;
@@ -19,69 +25,65 @@ const footerStyles = {
     }
   `,
   list: css`
-      list-style: none;
-      padding: 0;
-      display: flex;
-      gap: 1rem;
-      align-items: center;
-      flex-wrap: wrap;
+    list-style: none;
+    padding: 0;
+    display: flex;
+    gap: 1rem;
+    align-items: center;
+    flex-wrap: wrap;
   `,
   listItem: css`
-      font-size: 14px;
-      color: var(--gray-500);
+    font-size: 14px;
+    color: var(--gray-500);
   `,
   listItemText: css`
-      white-space: nowrap;
-      color: var(--gray-500);
+    white-space: nowrap;
+    color: var(--gray-500);
   `,
   divide: css`
-      display: inline-block;
-      height: 1rem;
-      border-right: 1px solid var(--gray-300);
+    display: inline-block;
+    height: 1rem;
+    border-right: 1px solid var(--gray-300);
   `,
   copyright: css`
-      font-size: 14px;
-      color: var(--gray-500);
+    font-size: 14px;
+    color: var(--gray-500);
   `,
 };
 
 const Footer = () => {
   return (
-    <footer css={footerStyles.container}>
-      <ul css={footerStyles.list}>
-        <li css={footerStyles.listItem}>
-          <Link
-            css={footerStyles.listItemText}
-            href="#"
-          >
-            이용약관
-          </Link>
-        </li>
-        <li css={footerStyles.divide} />
-        <li css={footerStyles.listItem}>
-          <Link
-            css={footerStyles.listItemText}
-            href="#"
-          >
-            개인정보 처리방침
-          </Link>
-        </li>
-        <li css={footerStyles.divide} />
-        <li css={footerStyles.listItem}>
-          <span css={footerStyles.listItemText}>고객문의&nbsp;</span>
-          <Link
-            css={css`
-                color: var(--main-red-500)
-            `}
-            href={'mailto:help@memopet.co.kr'}
-          >
-            help@memopet.co.kr
-          </Link>
-        </li>
-      </ul>
-      <span css={footerStyles.copyright}>
-        &copy; 2024 memopet. All rights reserved.
-      </span>
+    <footer css={styles.footer}>
+      <div css={styles.container}>
+        <ul css={styles.list}>
+          <li css={styles.listItem}>
+            <Link css={styles.listItemText} href='#'>
+              이용약관
+            </Link>
+          </li>
+          <li css={styles.divide} />
+          <li css={styles.listItem}>
+            <Link css={styles.listItemText} href='#'>
+              개인정보 처리방침
+            </Link>
+          </li>
+          <li css={styles.divide} />
+          <li css={styles.listItem}>
+            <span css={styles.listItemText}>고객문의&nbsp;</span>
+            <Link
+              css={css`
+                color: var(--main-red-500);
+              `}
+              href={'mailto:help@memopet.co.kr'}
+            >
+              help@memopet.co.kr
+            </Link>
+          </li>
+        </ul>
+        <span css={styles.copyright}>
+          &copy; 2024 memopet. All rights reserved.
+        </span>
+      </div>
     </footer>
   );
 };
