7f41fd799be54fa0ad415bc23af9248341aa0ab7
sh-oon <jshyh1@naver.com>
2024-06-09 22:56:19 +0900

    chore / theme 사용방법 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
-import { css } from '@emotion/react';
+import { css, useTheme } from '@emotion/react';
 
 export default function Home() {
+  const theme = useTheme()
+
   return (
     <div
       css={css`
         color: var(--grey-900);
+        background: ${theme.colors.primary['100']};
+        height: 100dvh;
       `}
     >
       main page
