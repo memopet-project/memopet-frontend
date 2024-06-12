a8e30784b66b53ad5211ea9e0552742a36fc9583
yujinyny <hyjinny00@gmail.com>
2024-06-12 18:26:12 +0900

    feat / ThumbMoreBadge 컴포넌트 추가

--- src/components/molecules/history/HistorySquareThumb.tsx
+++ src/components/molecules/history/HistorySquareThumb.tsx
@@ -1,34 +1,54 @@
 import { css, useTheme } from '@emotion/react';
 import Image from 'next/image';
 import sampleMemoryThumbnail from '@/assets/images/sampleMemoryThumbnail.png';
+import ThumbMoreBadge from '@/components/atoms/ThumbMoreBadge';
 
 interface IProps {
-  thumbImg: string;
+  thumbImgs: string[];
 }
 
-const HistorySquareTumnb = ({ thumbImg }: IProps) => {
+const HistorySquareTumnb = ({ thumbImgs }: IProps) => {
   const theme = useTheme();
   return (
     <div
       css={css`
+        width: fit-content;
         position: relative;
-        width: 240px;
-        height: 240px;
-        @media ${theme.device.mobile} {
-          width: 120px;
-          height: 120px;
-        }
       `}
     >
-      <Image
-        src={sampleMemoryThumbnail}
-        alt='썸네일 이미지'
-        layout='fill'
+      {thumbImgs.length > 1 ? (
+        <div
+          css={css`
+            z-index: 1;
+            position: absolute;
+            top: 8px;
+            right: 8px;
+          `}
+        >
+          <ThumbMoreBadge moreNum={thumbImgs.length - 1} />
+        </div>
+      ) : null}
+      <div
         css={css`
-          border-radius: 8px;
-          object-fit: cover;
+          position: relative;
+          width: 240px;
+          height: 240px;
+          @media ${theme.device.mobile} {
+            width: 120px;
+            height: 120px;
+          }
         `}
-      />
+      >
+        <Image
+          src={sampleMemoryThumbnail}
+          alt='썸네일 이미지'
+          layout='fill'
+          css={css`
+            border-radius: 8px;
+            object-fit: cover;
+          `}
+        />
+      </div>
     </div>
   );
 };
